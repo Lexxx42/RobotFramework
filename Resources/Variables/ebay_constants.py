34f03fdfda3114c5067f741c74330c6ae2936f55
Konyukhov Alexander <jesusmisha@gmail.com>
2024-05-01 16:38:13 +0300

    feature/variables - added use of list, dict and custom variables to the ebay tests - added test for delivery type - added module for custom types to the resources

---
+++ Resources/Variables/ebay_constants.py
@@ -0,0 +1,4 @@
+""" Module contains variables for eBay pages. """
+
+# Delivery filters chosen by user on the landing page: shown filters.
+AVAILABLE_DELIVERY_FILTERS = {"Бесплатная международная доставка": "Бесплатная доставка"}
