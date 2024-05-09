85cfa9346e3e46a26366efaa843a8754a0d7f147
Konyukhov Alexander <jesusmisha@gmail.com>
2024-05-09 11:53:06 +0300

    feature/ddt - added locators for login page - added example of data driven testing using table of test runs - rename of secret_reader.py function: name collision with selenium library

--- Resources/Locators/demoqa_locators.py
+++ Resources/Locators/demoqa_locators.py
@@ -2,3 +2,9 @@
 # Radio Button Page
 ELEMENT_BY_ID = "xpath://*[@id='{text}']/following-sibling::label"
 SUCCESS_TEXT = "css:.text-success"
+
+# Login page
+INPUT_USERNAME = "css:#userName"
+INPUT_PASSWORD = "xpath://*[@id='password']"
+BUTTON_LOGIN = "xpath://button[@id='login']"
+ERROR_MESSAGE_PAGE = "xpath://p[text()='Invalid username or password!']"
--- Resources/Utility/secret_reader.py
+++ Resources/Utility/secret_reader.py
@@ -9,7 +9,7 @@ load_dotenv(find_dotenv())
 
 
 @keyword
-def get_value(env_var_name: str) -> typing.Any:
+def get_env_value(env_var_name: str) -> typing.Any:
     """ Get environment variable from .env file by variable name.
 
     Args:
